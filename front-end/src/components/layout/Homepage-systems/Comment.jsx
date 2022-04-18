11c2f73fb3c0194d3bb6c516a606d001b6a1f513
andyfire9 <andyfire9@gmail.com>
2022-04-17 22:32:07 -0400

    Send up one last time

--- front-end/src/components/layout/Homepage-systems/Comment.jsx
+++ front-end/src/components/layout/Homepage-systems/Comment.jsx
@@ -30,9 +30,6 @@ const Comment = ({
   const createdAt = new Date(comment.createdAt).toLocaleDateString();
   return (
     <div key={comment.id} className="comment">
-      <div className="comment-image-container">
-        <img src="/user-icon.png" />
-      </div>
       <div className="comment-right-part">
         <div className="comment-content">
           <div className="comment-author">{comment.username}</div>
