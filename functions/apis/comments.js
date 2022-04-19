4bb633166f2e1c1f4905d01d66d607c902e42e7a
Juan Grullon <juangrullon1798@gmail.com>
2022-04-19 13:39:55 -0400

    comments fix and env file wasn't in the other push

--- functions/apis/comments.js
+++ functions/apis/comments.js
@@ -1,7 +1,6 @@
 
 const { db, firebase, config } = require("../utils/firebase");
 
-//Sirve, crea comentario en firestore
 
 const createComment = (req, res) => {
     const newComment = {
@@ -12,50 +11,49 @@ const createComment = (req, res) => {
         updatedAt: new Date().toISOString(),
 
     };
-    if (newComment.post === undefined) {
+    if (newComment.title == undefined) {
+        return res.status(400).json({ error: "Missing required parameter" });
+    }
+    if (newComment.postId == undefined) {
+        return res.status(400).json({ error: "Missing required parameter" });
+    }
+    if (newComment.username == undefined) {
         return res.status(400).json({ error: "Missing required parameter" });
     }
 
-    console.log("Starting Upload...", newComment.username);
-    db.collection(`/comments/`)
+    //TODO: validate the user publishing the post exists in the 
+    // database
+    console.log("Starting publishing comment...", newComment.username);
+    db.collection('comments')
         .add(newComment)
-        .then((doc) => {
-            return res.status(201).json({ message: "Comment created successfully" });
+        .then((docRef) => {
+            return res.status(201).json({ message: "post has been published", commentId: docRef.id });
         })
         .catch((error) => {
             res.status(500).json({ error: error.code });
         });
-        console.log(req);
 };
 
 
 const updatedComment = (req, res) => {
+
+    const authToken = req.headers.authorization;
+    if (!authToken) {
+        return res.status(401).json({ message: "not authenticated" });
+    }
     const comment = {
+        username: req.body.username,
+        postId: req.body.postId,
         post: req.body.post,
         title: req.body.title,
         updatedAt: new Date().toISOString(),
 
     };
-   const username = req.body.username;
 
-    if (comment.post == undefined) {
+    if (comment.postId == undefined) {
         return res.status(400).json({ error: "Missing requirement" });
     }
-
-    console.log("Post edited", username);
-    db.doc(`/comments/${username}`)
-    .update(comment)
-    .then((doc) => {
-        return res.status(200).json({ message: "Post updated successfully" });
-    })
-    .catch((error) => {
-        res.status(500).json({ error: error.code });
-        });
-};
-
-const deleteComment = (req,res) =>{
-    const commentId = req.params.commentId
-    db.doc(`/comment/${commentId}`)
+    db.doc(`/comments/${comment.postId}`)
     .get()
     .then((doc) => {
         if (!doc.exists) {
@@ -64,40 +62,58 @@ const deleteComment = (req,res) =>{
                 error: new Error(),
             };
         } else {
-            return db.doc (`/comment/${commentId}`).delete();
+            const existingComment = doc.data();
+
+            const updatedPost = {
+                username: post.username || existingComment.username,
+                title: post.title || existingComment.title,
+                tags: post.tags || existingComment.tags
+            };
+            return db.doc(`/comments/${comment.postId}`).update(updatedPost);
         }
     })
     .then(() => {
-        return res.status(200).json({ message: "Comment deleted successfully" });
+        return res.status(200).json({ message: "Post updated successfully" });
     })
     .catch((error) => {
         console.error(error);
         if (error.code === "comment/non-existent") {
             return res
                 .status(400)
-                .json({commentID: "Comment doesnt exist in the system" });
+                .json({ error: "comment doesn't exist in the system" });
         }
         res.status(500).json({ error: error.code });
     });
-}
+};
 
-//const deleteComment = (req, res) => {
-//    const delet = {
-//        username: req.body.username,
-//        title: req.body.title,
-//        post: req.body.post,
-//
-//    };
-//
-//    db.doc(`/comments/${delet.username}`)
-//        .delete(delet)
-//        .then((doc) => {
-//            return res.satus(200).json({ message: "comment deleted successfully" });
-//        })
-//        .catch((error) => {
-//            console.error(error);
-//            res.status(500).json({ error: error.code });
-//        });
-//}
+const deleteComment = (req, res) => {
+
+    const commentId = req.params.commentId;
+
+    db.doc(`/comments/${commentId}`)
+        .get()
+        .then((doc) => {
+            if (!doc.exists) {
+                throw {
+                    code: "comment/non-existent",
+                    error: new Error(),
+                };
+            } else {
+                return db.doc(`/comments/${commentId}`).delete();
+            }
+        })
+        .then(() => {
+            return res.status(200).json({ message: "comment deleted successfully" });
+        })
+        .catch((error) => {
+            console.error(error);
+            if (error.code === "comment/non-existent") {
+                return res
+                    .status(400)
+                    .json({ commentId: "comment doesn't exist in the system" });
+            }
+            res.status(500).json({ error: error.code });
+        });
+}
 
 module.exports = {createComment, updatedComment,  deleteComment  };
